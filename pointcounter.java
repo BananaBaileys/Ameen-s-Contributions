e0a669b8e4b9a6d8dc3f3d0f1f84b04e8dc1c355
BananaBaileys <61248008+BananaBaileys@users.noreply.github.com>
2020-02-20 13:42:33 -0500

    Add files via upload

---
+++ pointcounter.java
@@ -0,0 +1,11 @@
+package fuckthis;
+
+public class pointcounter {
+	public static void main(String[] args) {
+		int steps = 102;
+		double calculator = steps/20;
+		int points = (int) calculator;
+		System.out.println("Points gained: " + points);
+		
+}
+}
